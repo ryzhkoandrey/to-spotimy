6e77682cbf9648623065134d5267f440a88638f9
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-09 10:16:02 +0300

    +

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
 import { data } from './data.js';
+import { toMinAndSec } from './utils.js';
 
 const AudioController = {
    state: {
@@ -14,7 +15,32 @@ const AudioController = {
       this.audioList = document.querySelector('.items');
    },
 
-   loadAudioData(audio) {},
+   loadAudioData(audio) {
+      const { id, link, group, track, duration, genre } = audio;
+
+      const [image] = link.split('.');
+
+      const item = `
+         <div class="item" data-id="${id}">
+            <div class="item-image" style="background-image: url(./assets/images/${image}.jpg)"></div>
+
+            <div class="item-titles">
+               <h2 class="item-group">${group}</h2>
+               <h3 class="item-track">${track}</h3>
+            </div>
+
+            <p class="item-duration">${toMinAndSec(duration)}</p>
+
+            <p class="item-genre">${genre}</p>
+
+            <button class="item-play">
+               <svg class="icon-play">
+                  <use xlink:href="./assets/images/sprite.svg#play"></use>
+               </svg>
+            </button>
+         </div>
+      `;
+   },
 
    renderAudios() {
       data.forEach((item) => {
@@ -23,7 +49,7 @@ const AudioController = {
          audio.addEventListener('loadeddata', () => {
             const newItem = { ...item, duration: audio.duration, audio };
             this.state.audios.push(newItem);
-            loadAudioData(newItem);
+            this.loadAudioData(newItem);
          });
       });
    },
