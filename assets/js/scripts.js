79ce2370ae7a691c3caa54eff505d6d33c92a044
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-09 07:33:22 +0300

    +

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+import { data } from './data.js';
+
+const AudioController = {
+   init() {},
+
+   initVariables() {
+      this.audioList = document.querySelector('.items');
+   },
+};
+
+AudioController.init();
