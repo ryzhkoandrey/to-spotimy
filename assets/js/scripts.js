8bb0244893d7314f38dc79a0b4c37d063fb2f2a0
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-09 09:12:18 +0300

    +

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,31 @@
 import { data } from './data.js';
 
 const AudioController = {
-   init() {},
+   state: {
+      audios: [],
+   },
+
+   init() {
+      this.initVariables();
+      this.renderAudios();
+   },
 
    initVariables() {
       this.audioList = document.querySelector('.items');
+   },
+
+   loadAudioData(audio) {},
+
+   renderAudios() {
+      data.forEach((item) => {
+         const audio = new Audio(`./assets/audio/${item.link}`);
+
+         audio.addEventListener('loadeddata', () => {
+            const newItem = { ...item, duration: audio.duration, audio };
+            this.state.audios.push(newItem);
+            loadAudioData(newItem);
+         });
+      });
    },
 };
 
